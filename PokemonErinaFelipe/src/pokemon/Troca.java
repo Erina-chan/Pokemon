0e3a4db50342b7b05ebca33c4b19035476878bf3
Erina-chan <andrea95erina@hotmail.com>
2015-05-09 18:41:00 -0300

    Modificando Troca

--- PokemonErinaFelipe/src/pokemon/Troca.java
+++ PokemonErinaFelipe/src/pokemon/Troca.java
@@ -2,28 +2,39 @@ package pokemon;
 
 import java.util.Scanner;
 
-public class Troca implements Evento{
+public class Troca extends Evento{
 	
-	public void acao(Treinador t) {
-		Scanner sc = new Scanner(System.in);
-		boolean acao = imprimePokemons(t);
-		if (acao == true){
-			System.out.print("Digite o numero do pokemon que voce quer trocar: ");
-			int num = sc.nextInt() - 1;
-			t.trocaPokemon(t.pokebola[num]);
-			//sc.close();
-			System.out.println("O pokemon foi trocado.");
+	public Troca(int prioridade, Treinador ator, Treinador outro) {
+		super(prioridade, ator, outro);
+	}
+
+	public void acao() {
+		if (this.ator.perdeu == false && this.outro.perdeu == false){
+			Scanner sc = new Scanner(System.in);
+			boolean acao = imprimePokemons();
+			if (acao == true){
+				System.out.println(this.ator.nome);
+				System.out.print("Digite o numero do pokemon que voce quer trocar: ");
+				int num = sc.nextInt() - 1;
+				this.ator.trocaPokemon(this.ator.pokebola[num]);
+				System.out.println("O pokemon foi trocado.");
+				System.out.println();
+			}
 		}
 	}
 	
-	public static boolean imprimePokemons(Treinador t) {
+	public boolean imprimePokemons() {
 		boolean possibilidadeTroca = false;
+		System.out.println("Time Pokemon: ");
+		System.out.println("----------------------------------------------------------------------------------");
 		for(int i = 0; i < 6; i++){
-			if (t.pokebola[i] != null){
-				System.out.println(i + 1 + "..." + t.pokebola[i].nome + "... HP:" + t.pokebola[i].hp);
+			if (this.ator.pokebola[i] != null){
+				System.out.println(i + 1 + ". " + this.ator.pokebola[i].nome + " - HP:" + this.ator.pokebola[i].hp + "/100");
 				possibilidadeTroca = true;
 			}
 		}
+		System.out.println("----------------------------------------------------------------------------------");
+		System.out.println();
 		return possibilidadeTroca;
 	}
 	
