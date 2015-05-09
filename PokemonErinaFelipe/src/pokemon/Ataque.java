b5ec98fea5f438a7f3e0e9f64332067e98273d9d
Erina-chan <andrea95erina@hotmail.com>
2015-05-09 18:12:20 -0300

    Modificando Ataque

--- PokemonErinaFelipe/src/pokemon/Ataque.java
+++ PokemonErinaFelipe/src/pokemon/Ataque.java
@@ -6,73 +6,77 @@ public class Ataque {
 	String tipoAtaque; // Tipos do Pokemon: fogo, agua, choque, fisico.
 	int dano;
 	int prioridade;
-	
-	public Ataque(int numero){
-		if (numero == 1){
+
+	public Ataque(int numero) {
+		switch (numero) {
+		case 1:
 			this.nome = "Bomba Hidraulica";
 			this.tipoAtaque = "agua";
 			this.dano = 30;
-		}
-		else if (numero == 2){
+			break;
+		case 2:
 			this.nome = "Jato de Agua";
 			this.tipoAtaque = "agua";
 			this.dano = 20;
-		}
-		else if (numero == 3){
+			break;
+		case 3:
 			this.nome = "Rajada Flamejante";
 			this.tipoAtaque = "fogo";
 			this.dano = 30;
-		}
-		else if (numero == 4){
+			break;
+		case 4:
 			this.nome = "Bola de Fogo";
 			this.tipoAtaque = "fogo";
 			this.dano = 20;
-		}
-		else if (numero == 5){
+			break;
+		case 5:
 			this.nome = "Trovao";
 			this.tipoAtaque = "choque";
 			this.dano = 30;
-		}
-		else if (numero == 6){
+			break;
+		case 6:
 			this.nome = "Choque do Trovao";
 			this.tipoAtaque = "choque";
 			this.dano = 20;
-		}
-		else if (numero == 7){
+			break;
+		case 7:
 			this.nome = "Heavy Metal";
 			this.tipoAtaque = "metal";
 			this.dano = 30;
-		}
-		else if (numero == 8){
+			break;
+		case 8:
 			this.nome = "Tungstenio";
 			this.tipoAtaque = "metal";
 			this.dano = 20;
-		}
-		else if (numero == 9){
+			break;
+		case 9:
 			this.nome = "Ataque Rapido";
 			this.tipoAtaque = "fisico";
 			this.dano = 15;
-		}
-		else if (numero == 10){
+			break;
+		case 10:
 			this.nome = "Hyper Raio";
 			this.tipoAtaque = "fisico";
 			this.dano = 25;
-		}
-		else if (numero == 666){ 
+			break;
+		case 666:
 			// Easter Egg
 			this.nome = "Roubar Alma";
 			this.tipoAtaque = "spectral";
 			this.dano = 100;
-		}
-		else if (numero == 794){
+			break;
+		case 794:
 			// Easter Egg
 			this.nome = "Chan-chan-chan";
 			this.tipoAtaque = "samurai";
 			this.dano = 0;
+			break;
+		default:
+			break;
 		}
 	}
-	
-	public static void mostraAtaques(){
+
+	public static void mostraAtaques() {
 		System.out.println("\nAtaques disponiveis: ");
 		System.out.println("1. Bomba Hidraulica - agua - dano: 30 ");
 		System.out.println("2. Jato de Agua - agua - dano: 20 ");
